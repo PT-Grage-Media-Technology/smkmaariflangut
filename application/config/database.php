996065aaa7611257770e34179418dbf9ba9ec11d
RamadhanRendi18 <ramadhanprogramrendi01@gmail.com>
2024-10-08 10:53:50 +0700

    mencoba pm2

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'lokodinas',
+	'hostname' => '153.92.15.8',
+	'username' => 'u610515881_smkmaariflangu',
+	'password' => 'SmkMaarif@1',
+	'database' => 'u610515881_smkmaariflangu',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
---
+++ start-apache.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+php5.6 -S 0.0.0.0:8090 -t /var/www/html/smkmaariflangut
